797e0f2436b3ef95342f34117093fe8859041ac4
Eric Claus <ericclaus14@gmail.com>
2019-01-30 15:20:15 -0500

    add function names to psm1

--- Cyrus.psm1
+++ Cyrus.psm1
@@ -12,3 +12,45 @@
 .LINK
     https://github.com/ericclaus14/CyrusBackupSolution
 #>
+
+# Import required modules
+Import-Module Posh-SSH
+Import-Module 7Zip4PowerShell
+
+# Perform backups
+function Backup-VM {}
+function Backup-FileShare{}
+function Backup-SshAppliance{}
+function Backup-GroupPolicy{}
+function Backup-MSSQL {}
+
+# Automatically delete backups as per retention policies
+function Cleanup-Backup {}
+
+# Manage secure storage and retieval of passwords
+function Get-SecurePass {}
+function New-SecurePassFile {}
+
+# Read in and process the config file
+function Get-ConfigFile{}
+
+# Get the histories of each backup
+function Get-BackupFileHistory{}
+
+# Automatically assign drive letters for rotating external hard drives
+function Set-PartitionLetters{}
+
+# Dynamically create HTML pages for the web dashboard
+function Build-HtmlPages{}
+function Build-IndexPage{}
+function Build-HtmlContent{}
+
+# Manage the TFTP server
+function Start-TftpServer{}
+function Stop-TftpServer{}
+
+# Compare files for incremental backups
+function Compare-Files{}
+
+# Send alert emails upon backup failure
+function Send-AlertEmail{}
