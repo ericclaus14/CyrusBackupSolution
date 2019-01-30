5941319743f3fb466cfac1ed54e66d7e8ac6bf3c
Eric Claus <ericclaus14@gmail.com>
2019-01-30 15:00:15 -0500

    create barebone get-help comment block in .psm1 file

--- Cyrus.psm1
+++ Cyrus.psm1
@@ -1,1 +1,14 @@
-echo "Hi!!!!!!!!!!!!!!"
+<#
+.SYNOPSIS
+    Powershell module for the Cyrus Backup Solution
+
+    .DESCRIPTION
+    This module contains functions used for the Cyrus Backup Solution.
+
+.NOTES
+    Author: Eric Claus
+    Last Modified: 1/30/2019
+
+.LINK
+    https://github.com/ericclaus14/CyrusBackupSolution
+#>
